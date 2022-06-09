5bc1e8b3172dedcd37ff8d0b54106c11419111b4
Jason <jason.obeid98@gmail.com>
2022-06-09 10:04:51 -0400

    Use the correct channel id for the server

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 // Can get this in Discord. Settings > Advanced > Enable Developer mode.
 // Right click the text channel > Copy ID
-export const loungeTextChannelId = "714153671373619236";
+export const loungeTextChannelId = "940273271524442154";
